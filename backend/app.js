d1e4bd67b5e6e95e30c0d3c0c918fe3b93776278
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-19 19:07:27 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+connectDB();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
