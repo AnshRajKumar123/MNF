e46f3f3b85dc5c7d4070ebc19f10ff13de293a7b
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-23 12:45:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,48 @@
-import React from 'react'
-import 'boxicons'
-import HeroSect from './ComponentJSX/HeroSect'
-import { Route, Routes } from 'react-router-dom'
-import MainSection from './ComponentJSX/MainSection'
-import Footer from './ComponentJSX/Footer'
-import SignInUp from './ComponentJSX/SignInUp'
-import About from './PagesJSX/About'
-import HelpSupport from './PagesJSX/HelpSupport'
-import ReportFraud from './PagesJSX/ReportFraud'
-import ScrollToTop from './ComponentJSX/ScrollToTop'
-import Profile from './ComponentJSX/Profile'
-import FraudSuccess from './PagesJSX/FraudSuccess'
-import HelpSuccess from './PagesJSX/HelpSuccess'
-import TrackOrder from './PagesJSX/TrackOrder'
-import { Toaster } from "react-hot-toast";
-import ProtectedRoute from "./ComponentJSX/ProtectedRoute";
-import NotFound from './PagesJSX/NotFound'
-import OrderHistory from "./PagesJSX/OrderHistory";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import Users from "./pages/Users";
+import Coupons from "./pages/Coupons";
+import NotFound from "./pages/NotFound";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AdminLayout from "./components/layout/AdminLayout";
+
+function App() {
+
     return (
-        <>
-            <ScrollToTop />
-            <Toaster position="top-center" />
-            <Routes>
 
-                <Route path='/' element={<HeroSect />} />
-                <Route path='/signInUp' element={<SignInUp />} />
+        <Routes>
 
+            <Route path="/" element={<Login />} />
 
-                <Route element={<ProtectedRoute />}>
+            <Route element={<ProtectedRoute />}>
 
-                    <Route path='/mainWebsite/*' element={<MainSection />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/help-support' element={<HelpSupport />} />
-                    <Route path='/report-fraud' element={<ReportFraud />} />
-                    <Route path='/profile' element={<Profile />} />
-                    <Route path="/fraud-success" element={<FraudSuccess />} />
-                    <Route path="/help-success" element={<HelpSuccess />} />
-                    <Route path="/track-order/:orderId" element={<TrackOrder />} />
-                    <Route path="/order-history" element={<OrderHistory />} />
+                <Route element={<AdminLayout />}>
+
+                    <Route path="/dashboard" element={<Dashboard />} />
+
+                    <Route path="/products" element={<Products />} />
+
+                    <Route path="/orders" element={<Orders />} />
+
+                    <Route path="/users" element={<Users />} />
+
+                    <Route path="/coupons" element={<Coupons />} />
 
                 </Route>
 
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
-        </>
-    )
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+
+        </Routes>
+
+    );
+
 }
 
-export default App
+export default App;
