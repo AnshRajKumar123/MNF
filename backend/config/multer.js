0ae18db2c1b8c37df9510e284bd5bcd44f7dd66f
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-29 11:07:56 +0530

    comit

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,34 +1,16 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 const path = require("path");
-const fs = require("fs");
 
 const createUploader = (folderName) => {
 
-    const uploadPath = path.join(__dirname, "..", "uploads", folderName);
-
-    // Create folder if it doesn't exist
-    if (!fs.existsSync(uploadPath)) {
-        fs.mkdirSync(uploadPath, { recursive: true });
-    }
-
-    const storage = multer.diskStorage({
-
-        destination: (req, file, cb) => {
-            cb(null, uploadPath);
+    const storage = new CloudinaryStorage({
+        cloudinary,
+        params: {
+            folder: folderName,
+            allowed_formats: ["jpg", "jpeg", "png", "webp"],
         },
-
-        filename: (req, file, cb) => {
-
-            const uniqueName =
-                Date.now() + "-" + Math.round(Math.random() * 1e9);
-
-            cb(
-                null,
-                uniqueName + path.extname(file.originalname)
-            );
-
-        },
-
     });
 
     const fileFilter = (req, file, cb) => {
@@ -44,7 +26,7 @@ const createUploader = (folderName) => {
         if (isValid) {
             cb(null, true);
         } else {
-            cb(new Error("Only image files are allowed!"));
+            cb(new Error("Only image files are allowed."));
         }
 
     };
