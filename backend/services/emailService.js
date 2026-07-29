18ba3b136ee724f51aeea4b86b15ce6a00f357a5
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-29 07:27:00 +0530

    comit

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,14 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_PORT == 465, // false for port 587
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 10000, // 10 seconds timeout
 });
 
 transporter.verify((err) => {
