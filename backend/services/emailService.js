d66232f6d5587f2115e89157ac52746706823250
Ansh Kumar <golu.kkpncc.99008@gmail.com>
2026-07-31 22:22:31 +0530

    comit

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,24 +1,19 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
-
-    family: 4,          // 👈 Force IPv4
-
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     },
 });
 
 const sendEmail = async ({ to, subject, html }) => {
     try {
         const info = await transporter.sendMail({
-            from: `"MidNight Food" <${process.env.EMAIL_USER}>`,
+            from: '"MidNight Food" <b3f8b3001@smtp-brevo.com>',
             to,
             subject,
             html,
@@ -26,7 +21,6 @@ const sendEmail = async ({ to, subject, html }) => {
 
         console.log("✅ Email sent");
         console.log(info.messageId);
-        console.log(info);
 
         return info;
     } catch (error) {
